ed889a9410d3ba59b2d846431c344ef078353d2d
Danilo Silva <contact@danilocgsilva.me>
2021-02-13 17:12:50 -0300

    Added requirements, asking for argument

--- aws_costs_cli/__main__.py
+++ aws_costs_cli/__main__.py
@@ -1,7 +1,12 @@
 from aws_costs_api.AWSCosts import AWSCosts
+from danilocgsilvame_python_helpers.DcgsPythonHelpers import DcgsPythonHelpers
 
 def main():
+    
+    dcgsPHelpers = DcgsPythonHelpers()
+    args = dcgsPHelpers.command_line_argument_names('profile', 'p')
+    
     awscosts = AWSCosts()
+    awscosts.setProfile(args.profile)
     results = awscosts.getCosts()
     print(results)
-
