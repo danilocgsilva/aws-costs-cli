c42a24e1fd5b84a1b33335617df44bbbe1ba27ba
Danilo Silva <danilocgsilva@gmail.com>
2022-04-25 13:40:10 -0300

    Spread
    Does the working, but still with not friendly format

--- aws_costs_cli/__main__.py
+++ aws_costs_cli/__main__.py
@@ -1,8 +1,22 @@
 from aws_costs_api.AWSCosts import AWSCosts
+from numpy import result_type
 from aws_costs_cli.TerminalFormatter import TerminalFormatter
 from aws_costs_cli.CSV import CSV
 from aws_costs_cli.OutOfOptionException import OutOfOptionException
 import argparse
+
+serviceTranslationBag = {
+    "ec2": "EC2 - Other",
+    "s3": "Amazon Simple Storage Service",
+    "workmail": "AmazonWorkMail",
+    "tax": "Tax",
+    "cloudwatch": "AmazonCloudWatch",
+    "sns": "Amazon Simple Notification Service",
+    "route53": "Amazon Route 53",
+    "rds": "Amazon Relational Database Service",
+    "codecommit": "AWS CodeCommit",
+    "dynamodb": "Amazon DynamoDB"
+}
 
 def main():
     args = __get_arguments_parsed()
@@ -13,36 +27,31 @@ def main():
     if args.start_time:
         awscosts.setStartTime(args.start_time)
 
-    #if args.spread_services:
-    #    print("The services must be spreaded.")
-    #else:
-    #    print("The services may not be spreaded.")
-
     if args.types:
         for service in args.types.split(","):
             awscosts.setService(getServiceTranslation(service))
 
-    if args.format:
-        csvString = CSV().setAWSCostsClass(awscosts).get()
-        print(csvString)
+    if not args.spread_services:
+        if args.format:
+            csvString = CSV().setAWSCostsClass(awscosts).get()
+            print(csvString)
+        else:
+            TerminalFormatter().get(awscosts.getCosts())
     else:
-        TerminalFormatter().get(awscosts.getCosts())
-
+        for key_service in serviceTranslationBag:
+            awscosts.setUniqueService(getServiceTranslation(key_service))
+            results_by_time = awscosts.getCosts()["ResultsByTime"]
+            for single_result in results_by_time:
+                single_result["service"] = key_service
+                single_result["period"] = single_result["TimePeriod"]["Start"]
+                single_result.pop("TimePeriod", None)
+                single_result.pop("Estimated", None)
+                single_result.pop("Groups", None)
+                single_result["amount"] = single_result["Total"]["BlendedCost"]["Amount"]
+                single_result.pop("Total", None)
+                print(single_result)
 
 def getServiceTranslation(shortServiceName: str) -> str:
-
-    serviceTranslationBag = {
-        "ec2": "EC2 - Other",
-        "s3": "Amazon Simple Storage Service",
-        "workmail": "AmazonWorkMail",
-        "tax": "Tax",
-        "cloudwatch": "AmazonCloudWatch",
-        "sns": "Amazon Simple Notification Service",
-        "route53": "Amazon Route 53",
-        "rds": "Amazon Relational Database Service",
-        "codecommit": "AWS CodeCommit",
-        "dynamodb": "Amazon DynamoDB"
-    }
 
     if shortServiceName in serviceTranslationBag:
         return serviceTranslationBag[shortServiceName]
@@ -82,3 +91,14 @@ def __get_arguments_parsed():
         help="Shows the cost for each service."
     )
     return parser.parse_args()
+
+def __extract_single_result(raw: dict) -> dict:
+    time = raw["TimePeriod"]["Start"]
+    service = raw["Service"]
+    amount = raw["Total"]["BlendedCost"]["Amount"]
+
+    return {
+        "time": time,
+        "service": service,
+        "amount": amount
+    }
