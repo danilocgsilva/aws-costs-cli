f051e697aacc25dddad285e7a67f8b2223dbf807
Danilo Silva <danilocgsilva@gmail.com>
2022-06-11 20:15:16 -0300

    More specialized function methods
    On SpreadCalculator

--- aws_costs_cli/SpreadCalculator.py
+++ aws_costs_cli/SpreadCalculator.py
@@ -8,28 +8,31 @@ class SpreadCalculator:
 
     def set_client(self, client: AWSCosts):
         self.client = client
+        self.by_date_service_data = {}
 
     def get_data(self) -> dict:
+        self.__hidrate_raw_data()
+        self.__add_summarization_data()
+        return self.by_date_service_data
 
-        by_date_service_data = {}
+    def __hidrate_raw_data(self):
         for key_service in serviceTranslationBag:
             self.client.setUniqueService(getServiceTranslation(key_service))
             results_by_time = self.client.getCosts()["ResultsByTime"]
             for single_result in results_by_time:
                 single_result = self.__shrink_data(single_result, key_service)
 
-                if not single_result["period"] in by_date_service_data:
-                    by_date_service_data[single_result["period"]] = {}
+                if not single_result["period"] in self.by_date_service_data:
+                    self.by_date_service_data[single_result["period"]] = {}
 
-                by_date_service_data[single_result["period"]][key_service] = single_result["amount"]
+                self.by_date_service_data[single_result["period"]][key_service] = single_result["amount"]
 
-        for date in by_date_service_data:
+    def __add_summarization_data(self):
+        for date in self.by_date_service_data:
             date_sum = 0
-            for service_key in by_date_service_data[date]:
-                date_sum += by_date_service_data[date][service_key]
-            by_date_service_data[date]["TOTAL"] = date_sum
-
-        return by_date_service_data
+            for service_key in self.by_date_service_data[date]:
+                date_sum += self.by_date_service_data[date][service_key]
+            self.by_date_service_data[date]["TOTAL"] = date_sum
 
     def __shrink_data(self, raw: dict, key_service: str) -> dict:
 
